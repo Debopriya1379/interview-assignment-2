6366920ab49838c6d4ce94818fead81ffba95df5
Debopriya Das <debopriyadas721@gmail.com>
2022-08-30 00:27:22 +0530

    completed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
 import './App.css';
+import Map,{GeolocateControl,Marker} from 'react-map-gl';
+
+const mapboxAccessToken = process.env.MapboxAccessToken;
+
+const value1={
+  lng : 89+Math.random(),
+  lat : 26+Math.random(),
+}
+console.log(value1)
+
+const value2={
+  lng : 89+Math.random(),
+  lat : 26+Math.random(),
+}
+
+const value3={
+  lng : 89+Math.random(),
+  lat : 26+Math.random(),
+}
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <h1>Mapbox Assignment</h1> */}
+      <div className="map_container">
+        <Map
+            id="myMap"
+            initialViewState={{
+              longitude: 89.450996,
+              latitude: 26.323870,
+              zoom:8
+            }}
+            mapboxAccessToken={mapboxAccessToken}
+            mapStyle="mapbox://styles/mapbox/streets-v9"
+          >
+          <GeolocateControl/>
+          <Marker longitude={value1.lng} latitude={value1.lat} />
+          <Marker longitude={value2.lng} latitude={value2.lat} />
+          <Marker longitude={value3.lng} latitude={value3.lat} />
+        </Map>
+      </div>
     </div>
   );
 }
